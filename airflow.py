2f8cf79bb2f53efad0a46e0e3cda01f00f8e5c25
Bradley Fay <bradley.fay@gmail.com>
2016-12-03 13:48:53 +0000

    working test

--- airflow.py
+++ airflow.py
@@ -1,12 +1,17 @@
+from github_webhook import Webhook
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/gitpush', methods=['POST'])
-def deploy():
-    if request.method == 'POST':
-        d
-    else:
-        
+app = Flask(__name__)  # Standard Flask app
+webhook = Webhook(app) # Defines '/postreceive' endpoint
 
+@app.route("/")        # Standard Flask endpoint
+def hello_world():
+    return "Hello, World!"
 
+@webhook.hook()        # Defines a handler for the 'push' event
+def on_push(data):
+    print("Got push with: {0}".format(data))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
