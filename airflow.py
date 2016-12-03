cdf343cfdac4b77163614a61b1ed90edfd4a260f
Bradley Fay <bradley.fay@gmail.com>
2016-12-03 09:44:22 -0500

    Add functionality and logging

--- airflow.py
+++ airflow.py
@@ -1,8 +1,17 @@
+import logging
+import subprocess
+
 from github_webhook import Webhook
 from flask import Flask
+from git import Repo
 
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
+
+repo = Repo("/home/ubuntu/airflow/dags/")
+
+restart_webserver = ['sudo', 'initctl', 'restart', 'airflow-webserver']
+restart_scheduler = ['sudo', 'initctl', 'restart', 'airflow-scheduler']
 
 @app.route("/")        # Standard Flask endpoint
 def hello_world():
@@ -10,7 +19,29 @@ def hello_world():
 
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
-    print("Got push with: {0}".format(data))
+	if data['ref'].lstrip('refs/heads/') == 'master':
+		logging.info("Checking out master")
+    	repo.heads.master.checkout()
+
+    	logging.info("Pulling updates to master")
+    	repo.git.pull()
+
+    	logging.info("Restarting Webserver")
+    	subprocess.check_call(restart_webserver)
+
+    	logging.info("Restarting Scheduler")
+    	subprocess.check_call(restart_scheduler)
+    else:
+    	logging.info('Received Push from {0}'.format(data['ref'].lstrip('refs/heads/')))
+
+
+
+logging.getLogger(__name__).addHandler(logging.StreamHandler())
+logging.basicConfig(filename='githooks.log', 
+	                format='%(asctime)s - %(levelname)s:%(message)s',
+	                datefmt='%m-%d-%Y %I:%M:%S %p',
+	                level=logging.INFO)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
