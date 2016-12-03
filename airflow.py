3b02c87d86757d5dd7ff9396666cb21efdd11053
Bradley Fay <bradley.fay@gmail.com>
2016-12-03 09:45:56 -0500

    tabs to spaces

--- airflow.py
+++ airflow.py
@@ -19,28 +19,28 @@ def hello_world():
 
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
-	if data['ref'].lstrip('refs/heads/') == 'master':
-		logging.info("Checking out master")
-    	repo.heads.master.checkout()
+    if data['ref'].lstrip('refs/heads/') == 'master':
+        logging.info("Checking out master")
+        repo.heads.master.checkout()
 
-    	logging.info("Pulling updates to master")
-    	repo.git.pull()
+        logging.info("Pulling updates to master")
+        repo.git.pull()
 
-    	logging.info("Restarting Webserver")
-    	subprocess.check_call(restart_webserver)
+        logging.info("Restarting Webserver")
+        subprocess.check_call(restart_webserver)
 
-    	logging.info("Restarting Scheduler")
-    	subprocess.check_call(restart_scheduler)
+        logging.info("Restarting Scheduler")
+        subprocess.check_call(restart_scheduler)
     else:
-    	logging.info('Received Push from {0}'.format(data['ref'].lstrip('refs/heads/')))
+        logging.info('Received Push from {0}'.format(data['ref'].lstrip('refs/heads/')))
 
 
 
 logging.getLogger(__name__).addHandler(logging.StreamHandler())
 logging.basicConfig(filename='githooks.log', 
-	                format='%(asctime)s - %(levelname)s:%(message)s',
-	                datefmt='%m-%d-%Y %I:%M:%S %p',
-	                level=logging.INFO)
+                    format='%(asctime)s - %(levelname)s:%(message)s',
+                    datefmt='%m-%d-%Y %I:%M:%S %p',
+                    level=logging.INFO)
 
 
 if __name__ == "__main__":
